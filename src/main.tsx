cbaf7ff57706d7220f219f5d2e87ec6479e6783b
Priyanshu Yadav <114854534+ipriyanshu25@users.noreply.github.com>
2025-08-22 11:14:22 +0530

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,3 @@
-// import { StrictMode } from 'react';
-// import { createRoot } from 'react-dom/client';
-// import { BrowserRouter } from 'react-router-dom';
-// import App from './App';
-// import './index.css';
-
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </StrictMode>
-// );
-
 // src/main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
